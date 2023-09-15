3e4f72eb511ee17988e2d4e718c8aaaa4efa2135
Sadick <122355398+Sadickachuli@users.noreply.github.com>
2023-09-15 17:31:27 +0200

    minOPS

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -3,7 +3,7 @@
 This module provides a function to calculate the minimum number of operations to get 'n' H characters.
 """
 
-def minOperations(n):
+def min_operations(n):
     """
     Calculate the fewest number of operations needed to result in exactly 'n' H characters in the file.
 
@@ -15,10 +15,8 @@ def minOperations(n):
     """
     if n <= 1:
         return 0
-
     operations = 0
     divisor = 2
-
     while n > 1:
         while n % divisor == 0:
             operations += divisor
