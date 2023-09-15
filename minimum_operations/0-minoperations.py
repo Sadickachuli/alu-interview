5261466fd8fe506074711bfe914c61635ff0aa15
Sadickachuli <m.achuli@alustudent.com>
2023-09-15 16:48:44 +0200

    minOP

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    dp = [float('inf')] * (n + 1)
+    dp[1] = 0
+
+    for i in range(2, n + 1):
+        for j in range(2, int(i ** 0.5) + 1):
+            if i % j == 0:
+                dp[i] = min(dp[i], dp[j] + i // j)
+                break
+
+        if dp[i] == float('inf'):
+            dp[i] = i
+
+    return dp[n]
+
+if __name__ == "__main__":
+    n = 4
+    print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+    n = 12
+    print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
