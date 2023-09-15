000c1e0b21b6c753d24ab33674c6e15acdc9b4fd
Sadickachuli <m.achuli@alustudent.com>
2023-09-15 17:00:21 +0200

    minOP

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -13,7 +13,6 @@ def minOperations(n):
     Returns:
         int: The minimum number of operations needed to reach 'n' H characters. If 'n' is impossible to achieve, return 0.
     """
-
     if n <= 1:
         return 0
 
@@ -24,8 +23,8 @@ def minOperations(n):
         for j in range(2, int(i ** 0.5) + 1):
             if i % j == 0:
                 dp[i] = min(dp[i], dp[j] + i // j)
-                break
-
+                if i // j != j:
+                    dp[i] = min(dp[i], dp[i // j] + j)
         if dp[i] == float('inf'):
             dp[i] = i
 
@@ -37,4 +36,3 @@ if __name__ == "__main__":
 
     n = 12
     print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
-
