2baf1b03022cf6490959a10204b534812044c639
Sadick <122355398+Sadickachuli@users.noreply.github.com>
2023-09-15 17:54:25 +0200

    minOPss

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -7,12 +7,13 @@ minimum number of operations to get 'n' H characters.
 
 def minOperations(n):
     """
-    Calculate the fewest number of operations needed to result in exactly 'n' H characters in the file.
+    Calculate the fewest number of operations needed to result in exactly 'n' H 
+    characters in the file.
     Args:
         n (int): The target number of H characters.
     Returns:
-        int: The minimum number of operations needed to 
-        reach 'n' H characters. If 'n' is impossible to achieve, return 0.
+        int: The minimum number of operations needed to reach 'n' H characters. If
+        'n' is impossible to achieve, return 0.
     """
     if n <= 1:
         return 0
